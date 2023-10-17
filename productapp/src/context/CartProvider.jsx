3b7652d8dae759f85121a2521b7eb405cc76dc07
Banuprakash <banuprakashc@yahoo.co.in>
2023-10-17 14:29:05 +0530

    day 6

--- productapp/src/context/CartProvider.jsx
+++ productapp/src/context/CartProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useReducer } from "react";
 import cartReducer from "../reducers/cartReducer";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const CartContext = createContext();
 
 const initialState = {
@@ -8,26 +9,39 @@ const initialState = {
     total: 0
 }
 export default function CartProvider(props) {
+    let navigate = useNavigate();
     let [state, dispatch] = useReducer(cartReducer, initialState);
 
     function addToCart(product) {
-        dispatch({type: 'ADD_TO_CART', payload: product});
+        dispatch({ type: 'ADD_TO_CART', payload: product });
     }
 
     function increment(id) {
-        dispatch({type:'INCREMENT', payload: id});
+        dispatch({ type: 'INCREMENT', payload: id });
     }
 
     function checkout() {
-        dispatch({type:'CLEAR_CART'});
+        //window.sessionStorage.setItem("user", "banu@gmail.com"); //login success
+        let order = {};
+        order.customer = window.sessionStorage.getItem("user");
+        order.total = state.total;
+        order.items = state.cartItems;
+
+        axios.post("http://localhost:1234/orders", order).then(response => {
+            console.log("Order placed!!!");
+            dispatch({ type: 'CLEAR_CART' });
+            navigate("/");
+        })
+
     }
 
     return <CartContext.Provider value={{
-                products: state.cartItems, 
-                total: state.total,
-                addToCart, 
-                increment, 
-                checkout}}>
+        products: state.cartItems,
+        total: state.total,
+        addToCart,
+        increment,
+        checkout
+    }}>
         {props.children}
     </CartContext.Provider>
 }
