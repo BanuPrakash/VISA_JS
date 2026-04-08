278a606642e3169b6bee9a392c970172af600de4
Banuprakash <banuprakashc@yahoo.co.in>
2026-04-08 15:37:11 +0530

    Shop application

--- workspace/shopapp/src/products/ProductList.jsx
+++ workspace/shopapp/src/products/ProductList.jsx
@@ -1,7 +1,8 @@
 
 export default function ProductList() {
   let [products, setProducts] = useState([]);
-  
+  // products is state
+  // always mutate products using setProducts() function
   return (
     <div>ProductList</div>
   )
