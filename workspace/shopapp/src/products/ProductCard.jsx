d855ca5e5817fdf808f67485480778da53c770bf
Banuprakash <banuprakashc@yahoo.co.in>
2026-04-09 10:07:46 +0530

    Shop app

--- workspace/shopapp/src/products/ProductCard.jsx
+++ workspace/shopapp/src/products/ProductCard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
-export default function ProductCard() {
+
+export default function ProductCard({product}) {
+  let {id, title, price, description, images} = product;
   return (
-    <div>ProductCard</div>
+    <div className="col-sm-6 col-md-4">
+       <Card>
+        <Card.Img variant="top" src={images[0]} />
+        <Card.Header>{title}</Card.Header>
+        <Card.Body>
+          <Card.Text>
+           {description}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          Price ${price} &nbsp;
+          <Button variant='success'>Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
   )
 }
--- workspace/shopapp/src/products/ProductList.jsx
+++ workspace/shopapp/src/products/ProductList.jsx
@@ -1,9 +1,35 @@
-
+import { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import {Container} from 'react-bootstrap'
 export default function ProductList() {
-  let [products, setProducts] = useState([]);
+  let [products, setProducts] = useState();
+  let [count, setCount] = useState();
+
+  // componentDidMount()
+  // called only once
+  useEffect(() => {
+    fetch("https://dummyjson.com/products?limit=6")
+    .then(response => response.json())
+    .then(data => setProducts(data.products));
+  },[]); // empty depdenency
+
+  // componentDidUpdate
+  // gets called whenever count changes
+   useEffect(() => {
+
+   }, [count]); 
+
   // products is state
   // always mutate products using setProducts() function
   return (
-    <div>ProductList</div>
+    <Container>
+      <div className="row">
+        <h1>Product List!!!</h1>
+          {
+          products && products.map(product => <ProductCard product={product}/>)
+        }
+      </div>
+    
+    </Container>
   )
 }
